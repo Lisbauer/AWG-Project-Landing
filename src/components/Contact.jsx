48079eb9eddcfbeafd3250ace8dd99d51d06a0e0
Lisbauer <lisa.bauer@hotmail.com>
2023-05-15 13:56:03 -0300

    country dropdown, service dropdown, video section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,54 @@
-import React from "react";
+import React, { useState,  useMemo  } from "react";
 import "./contact.css";
 import TextField from "@mui/material/TextField";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import IconButton from '@mui/material/IconButton';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
+import countryList from 'react-select-country-list'
 import "./navbar.css";
+import Select from "@mui/material/Select";
+import FormControl from "@mui/material/FormControl";
+import InputLabel from "@mui/material/InputLabel";
+import ListItemText from '@mui/material/ListItemText';
+import { styled } from '@mui/material/styles';
+
+const SelectRoot = styled(Select)({
+  "& .MuiInput-underline:before, & .MuiInput-underline:after": {
+    display: "none",
+  },
+  textAlign: 'left',
+  "& .MuiSelect-select": {
+    paddingRight: '32px',
+  },
+});
+
 
 const Contact = () => {
+  const [anchorEl, setAnchorEl] = useState(null);
+  const [selectedCountry, setSelectedCountry] = useState(null);
+  const [selectedService, setSelectedService] = useState(null);
+
+  const options = useMemo(() => countryList().getData(), [])
+
+
+  const handleOpenMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleCloseMenu = () => {
+    setAnchorEl(null);
+  };
+
+  const handleCountrySelect = (country) => {
+    setSelectedCountry(country);
+    handleCloseMenu();
+  };
+
+  const handleServiceSelect = (service) => {
+    setSelectedService(service);
+    handleCloseMenu();
+  };
   return (
     <div className="contact-container">
       <section className="business-boost-section">
@@ -64,13 +109,35 @@ const Contact = () => {
               multiline
               maxRows={4}
             />
-            <TextField
-              sx={{ width: "450px", marginBottom: "28px" }}
-              id="outlined-multiline-flexible"
-              label="Pais"
-              multiline
-              maxRows={4}
-            />
+  <FormControl sx={{ width: "450px", marginBottom: "28px" }}>
+              <InputLabel id="country-label">Pais</InputLabel>
+              <SelectRoot
+                labelId="country-label"
+                value={selectedCountry}
+                onChange={(e) => handleCountrySelect(e.target.value)}
+                renderValue={(selected) => selected ? selected.label : ""}
+                MenuProps={{
+                  anchorOrigin: {
+                    vertical: "bottom",
+                    horizontal: "left",
+                  },
+                  transformOrigin: {
+                    vertical: "top",
+                    horizontal: "left",
+                  },
+                  getContentAnchorEl: null,
+                }}
+              >
+                {options.map((option) => (
+                  <MenuItem
+                    key={option.value}
+                    value={option}
+                  >
+                    <ListItemText primary={option.label} />
+                  </MenuItem>
+                ))}
+              </SelectRoot>
+            </FormControl>
             <TextField
               sx={{ width: "450px", marginBottom: "28px" }}
               id="outlined-multiline-flexible"
@@ -78,19 +145,55 @@ const Contact = () => {
               multiline
               maxRows={4}
             />
+
             <TextField
               sx={{ width: "450px", marginBottom: "28px" }}
               id="outlined-multiline-flexible"
               label="Servicio de interés"
-              multiline
-              maxRows={4}
+              value={selectedService}
+              onChange={(e) => setSelectedService(e.target.value)}
+              onClick={handleOpenMenu}
+              InputLabelProps={{
+                shrink: selectedService ? true : undefined,
+              }}
+              InputProps={{
+                endAdornment: (
+                  <IconButton size="small" onClick={handleOpenMenu}>
+                    <ArrowDropDownIcon />
+                  </IconButton>
+                )
+              }}
             />
+            <Menu
+              anchorEl={anchorEl}
+              open={Boolean(anchorEl)}
+              onClose={handleCloseMenu}
+            >
+              <MenuItem onClick={() => handleServiceSelect("ABM de productos")}>
+                ABM de productos
+              </MenuItem>
+              <MenuItem onClick={() => handleServiceSelect("Retail Readiness")}>
+                Retail Readiness
+              </MenuItem>
+              <MenuItem
+                onClick={() =>
+                  handleServiceSelect("Gestión de cuentas publicitarias")
+                }
+              >
+                Gestión de cuentas publicitarias
+              </MenuItem>
+            </Menu>
+
           </div>
           <div className="btn-wp">
-          <img className="whatsapp-logo" src="../images/whatsapplogo.png" alt="wpplogo" />
-          <button className="message-button">
-            <strong>Enviar mensaje</strong>
-          </button>
+            <img
+              className="whatsapp-logo"
+              src="../images/whatsapplogo.png"
+              alt="wpplogo"
+            />
+            <button className="message-button">
+              <strong>Enviar mensaje</strong>
+            </button>
           </div>
         </div>
       </section>
--- src/components/Videos.jsx
+++ src/components/Videos.jsx
@@ -1,49 +1,67 @@
 import React from 'react'
 import './videos.css'
+import YouTube from 'react-youtube';
+
 
 const Videos = () => {
   return (
     <div>
-      <section>
-        <h2>CONOCE MÁS</h2>
+      <section className='video-container'>
+        <h2>CONOCÉ <span className='mas-span'>MÁS</span></h2>
 
-        <div>
-          <div>
+        <div className='video-box'>
+        <YouTube videoId="_yqSbnbUsj4" />
+          <div className='text-box-right'>
             <h3>¿Cómo abrir una cuenta publicitaria en Amazon?</h3>
             <h4>¿Qué tipo de campañas se pueden gestionar desde ahí?</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas scelerisque lectus lorem, id auctor lectus finibus ac. In hac habitasse platea dictumst...</p>
           </div>
         </div>
 
-        <div>
-          <div>
+        <div className='video-box'>
+          <div className='text-box-left'>
             <h3>¿Cómo dar de alta un producto?</h3>
             <h4> ¿Cuáles se pueden publicitar y cuáles no?</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas scelerisque lectus lorem, id auctor lectus finibus ac. In hac habitasse platea dictumst...</p>
-          </div>
+          </div>  
+          <YouTube videoId="_yqSbnbUsj4" />
         </div>
 
-        <div>
-          <div>
+        <div className='video-box'>
+        <YouTube videoId="_yqSbnbUsj4" />
+          <div className='text-box-right'>
             <h3>¿Cómo generar ventas en la plataforma?</h3>
             <h4> Ejemplo Amazon</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas scelerisque lectus lorem, id auctor lectus finibus ac. In hac habitasse platea dictumst...</p>
           </div>
         </div>
 
-        <div>
-          <div>
+        <div className='video-box'>
+       
+          <div className='text-box-left'>
             <h3>¿Cómo se usan los descuentos en las plataformas?</h3>
+            <h4>Oferta vs Cupón vs Promoción</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas scelerisque lectus lorem, id auctor lectus finibus ac. In hac habitasse platea dictumst...</p>
+          </div> 
+          <YouTube videoId="_yqSbnbUsj4" />
+        </div>
+
+        <div className='video-box'>
+        <YouTube videoId="_yqSbnbUsj4" />
+          <div className='text-box-right'>
+            <h3>¿Cuáles son los servicios de AWG Retail Media?</h3>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas scelerisque lectus lorem, id auctor lectus finibus ac. In hac habitasse platea dictumst...</p>
           </div>
         </div>
 
-        <div>
-          <div>
+        <div className='video-box'>
+       
+          <div className='text-box-left'>
             <h3>Glosario</h3>
             <h4>Conceptos básicos que deberías saber antes de incursionar en una plataforma de e-commerce</h4>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas scelerisque lectus lorem, id auctor lectus finibus ac. In hac habitasse platea dictumst...</p>
-          </div>
+          </div> 
+          <YouTube videoId="_yqSbnbUsj4" />
         </div>
       </section>
     </div>
